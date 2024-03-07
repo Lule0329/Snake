392e77293e9ef5588411d909a43822fe59705296
Ludwig Lennartsson <ludwig.lennartsson@skola.sala.se>
2024-03-07 09:17:41 +0100

    Add project files.

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.snake = new System.Windows.Forms.PictureBox();
+            this.food = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.snake)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // snake
+            // 
+            this.snake.BackColor = System.Drawing.Color.Lime;
+            this.snake.Location = new System.Drawing.Point(0, 0);
+            this.snake.Name = "snake";
+            this.snake.Size = new System.Drawing.Size(100, 100);
+            this.snake.TabIndex = 0;
+            this.snake.TabStop = false;
+            // 
+            // food
+            // 
+            this.food.BackColor = System.Drawing.Color.Red;
+            this.food.Location = new System.Drawing.Point(106, 0);
+            this.food.Name = "food";
+            this.food.Size = new System.Drawing.Size(100, 100);
+            this.food.TabIndex = 1;
+            this.food.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Score: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.food);
+            this.Controls.Add(this.snake);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Snake";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.snake)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox snake;
+        private System.Windows.Forms.PictureBox food;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Snake/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        const int SIZE_SNAKE = 32;
+        const int SIZE_FOOD = 16;
+
+        List<PictureBox> tailpieces = new List<PictureBox>(); // Håller koll på svansbitarna
+        Random rng = new Random();
+        int i = 0;
+        string direction = "Right"; // Håller koll på vilken riktning ormen pekar
+        int score; // Antal poäng
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void addTail()
+        {
+            // Skapa en ny pictureBox
+            PictureBox box = new PictureBox();
+            // Sätt alla egenskapers som behövs 
+            box.Location = snake.Location;
+            box.Size = snake.Size;
+            box.BackColor = snake.BackColor;
+
+            // Lägg in lådan i controls så att fönstret visar den 
+            Controls.Add(box);
+
+            // Lägg till lådan i tailPieces så vi kan manipulera dem
+            tailpieces.Add(box);
+        }
+
+        void moveTail()
+        {
+            if (tailpieces.Count > 0)
+            {
+                tailpieces[i].Location = snake.Location;
+
+                i = i + 1;
+
+                if (i >= tailpieces.Count)
+                {
+                    i = 0;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ClientSize = new Size(SIZE_SNAKE * 20, SIZE_SNAKE * 15);
+            snake.Size = new Size(SIZE_SNAKE, SIZE_SNAKE);
+            food.Size = new Size(SIZE_FOOD, SIZE_FOOD);
+
+            food.Location = new Point(rng.Next(0, ClientSize.Width - SIZE_FOOD), rng.Next(0, ClientSize.Height - SIZE_FOOD));
+            snake.Location = new Point(0, 0);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'a')
+            {
+                direction = "left";
+            }
+            else if (e.KeyChar == 'd')
+            {
+                direction = "right";
+            }
+            else if (e.KeyChar == 'w')
+            {
+                direction = "up";
+            }
+            else if (e.KeyChar == 's')
+            {
+                direction = "down";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveTail();
+
+            if (direction == "left")
+            {
+                snake.Left -= SIZE_SNAKE;
+            }
+            else if (direction == "right")
+            {
+                snake.Left += SIZE_SNAKE;
+            }
+            else if (direction == "up")
+            {
+                snake.Top -= SIZE_SNAKE;
+            }
+            else if (direction == "down")
+            {
+                snake.Top += SIZE_SNAKE;
+            }
+
+            if (snake.Bounds.IntersectsWith(food.Bounds))
+            {
+                food.Location = new Point(rng.Next(0, ClientSize.Width - SIZE_FOOD), rng.Next(0, ClientSize.Height - SIZE_FOOD));
+                addTail();
+                score += 1;
+                label1.Text = "Score: " + score;
+            }
+            
+            if (snake.Bounds.IntersectsWith(tailpieces.Bounds))
+            {
+                timer1.Stop();
+                MessageBox.Show("You Lost!");
+            }
+        }
+    }
+}
